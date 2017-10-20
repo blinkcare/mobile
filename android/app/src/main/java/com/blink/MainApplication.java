f27b556667faee554ab554bb025e4ba955922b15
kusti8 <kusti8@gmail.com>
2017-10-20 13:37:57 -0700

    Unlink all packages

--- android/app/src/main/java/com/blink/MainApplication.java
+++ android/app/src/main/java/com/blink/MainApplication.java
@@ -3,9 +3,6 @@ package com.blink;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,10 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNSpinkitPackage(),
-            new ReactNativeConfigPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
   };
