a0f310f8d51c8bc23b9605c0e1b9c17ff48c9de7
kusti8 <kusti8@gmail.com>
2017-10-20 14:00:41 -0700

    Upgrade react-native

--- android/app/src/main/java/com/blink/MainApplication.java
+++ android/app/src/main/java/com/blink/MainApplication.java
@@ -3,6 +3,9 @@ package com.blink;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,8 +25,16 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNSpinkitPackage(),
+            new ReactNativeConfigPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
